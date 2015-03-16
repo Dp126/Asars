6d2ff32b8a70d63672e1374df418b6b264e5cc64
hanniestars <hanniemarquez@gmail.com>
2015-03-16 17:38:45 +0800

    added assocation product and sale

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316072905) do
+ActiveRecord::Schema.define(version: 20150316092424) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "agency_name"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20150316072905) do
     t.decimal  "incentive"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "product_id"
   end
 
   create_table "users", force: :cascade do |t|
